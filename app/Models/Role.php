08ef6f69b92508eb5c12c610e01d0e7beb5dd9fb
Ibnu Surkati <dev.surkati@gmail.com>
2020-03-03 17:15:26 +0800

    add softdelete in user

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
+
+    use SoftDeletes;
 
     protected $table = 'roles';
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use App\Models\ProfileSantri;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
