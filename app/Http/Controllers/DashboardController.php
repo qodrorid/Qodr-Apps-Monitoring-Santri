d67495816e7bd6e3c13181cc40564943ef2f57e1
Ibnu Surkati <dev.surkati@gmail.com>
2020-03-06 17:37:19 +0800

    add multi dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,105 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+use Auth;
+use DB;
 
 class DashboardController extends Controller
 {
     
     public function index()
     {
-        return view('pages.dashboard.admin');
+        $role = Auth::user()->role_id;
+
+        switch ($role) {
+            case 1:
+                # Super User
+                return $this->superUser();
+            case 2:
+                # Admin
+                return $this->admin();
+            case 3:
+                # Ketua
+                return $this->ketua();
+            case 4:
+                # Sekretaris
+                return $this->sekretaris();
+            case 5:
+                # Bendahara
+                return $this->bendahara();
+            case 6:
+                # Divisi IT
+                return $this->divisiIT();
+            case 7:
+                # Divisi Agama
+                return $this->divisiAgama();
+            case 8:
+                # Mitra
+                return $this->mitra();
+            case 9:
+                # Santri
+                return $this->santri();
+        }
     }
+
+    private function superUser()
+    {
+        $logs  = Storage::disk('logs')->files();
+        $roles = DB::table('roles')->whereNull('deleted_at')->count();
+        $users = DB::table('users')->whereNull('deleted_at')->count();
+        
+        unset($logs[0]);
+        
+        $widget = (object) [
+            'logs' => count($logs),
+            'role' => $roles,
+            'user' => $users,
+        ];
+
+        return view('pages.dashboard.1', compact('widget'));
+    }
+
+    private function admin()
+    {
+        return view('pages.dashboard.example');
+    }
+
+    private function ketua()
+    {
+        return view('pages.dashboard.example');
+    }
+
+    private function sekretaris()
+    {
+        return view('pages.dashboard.example');
+    }
+
+    private function bendahara()
+    {
+        return view('pages.dashboard.example');
+    }
+
+    private function divisiIT()
+    {
+        return view('pages.dashboard.example');
+    }
+
+    private function divisiAgama()
+    {
+        return view('pages.dashboard.example');
+    }
+
+    private function mitra()
+    {
+        return view('pages.dashboard.example');
+    }
+
+    private function santri()
+    {
+        return view('pages.dashboard.example');
+    }
+
 
 }
---
+++ resources/views/pages/dashboard/1.blade.php
@@ -0,0 +1,73 @@
+@extends('templates.base')
+
+@section('content')
+
+@include('components.page-header', [
+    'title' => 'Dashboard',
+    'subtitle' => 'Control Panel',
+    'breadcrumb' => [
+        'dashboard'
+    ]
+])
+
+<div class="page-body">
+    <div class="row">
+        <div class="col-md-6 col-xl-3">
+            <div class="card widget-card-1">
+                <div class="card-block-small">
+                    <i class="feather icon-alert-triangle bg-c-pink card1-icon"></i>
+                    <span class="text-c-pink f-w-600">Error Logs</span>
+                    <h4>{{ $widget->logs }} Logs</h4>
+                    <div>
+                        <span class="f-left m-t-10 text-muted">
+                            <i class="text-c-pink f-16 feather icon-link m-r-10"></i>more then ...
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-3">
+            <div class="card widget-card-1">
+                <div class="card-block-small">
+                    <i class="feather icon-pie-chart bg-c-green card1-icon"></i>
+                    <span class="text-c-green f-w-600">Role</span>
+                    <h4>{{ $widget->role }} Access</h4>
+                    <div>
+                        <span class="f-left m-t-10 text-muted">
+                            <i class="text-c-green f-16 feather icon-link m-r-10"></i>more then ...
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-3">
+            <div class="card widget-card-1">
+                <div class="card-block-small">
+                    <i class="feather icon-pie-chart bg-c-yellow card1-icon"></i>
+                    <span class="text-c-yellow f-w-600">Users</span>
+                    <h4>{{ $widget->user }} Users</h4>
+                    <div>
+                        <span class="f-left m-t-10 text-muted">
+                            <i class="text-c-yellow f-16 feather icon-link m-r-10"></i>more then ...
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-3">
+            <div class="card widget-card-1">
+                <div class="card-block-small">
+                    <i class="feather icon-pie-chart bg-c-blue card1-icon"></i>
+                    <span class="text-c-blue f-w-600">Branch</span>
+                    <h4>2 Branch</h4>
+                    <div>
+                        <span class="f-left m-t-10 text-muted">
+                            <i class="text-c-blue f-16 feather icon-link m-r-10"></i>more then ...
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
