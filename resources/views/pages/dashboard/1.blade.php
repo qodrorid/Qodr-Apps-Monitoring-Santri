3155b19be78df33f5ce9281cf4ece25fd8463c34
Ibnu Surkati <dev.surkati@gmail.com>
2020-03-06 17:39:07 +0800

    fix icon dashboard

--- resources/views/pages/dashboard/1.blade.php
+++ resources/views/pages/dashboard/1.blade.php
@@ -29,7 +29,7 @@
         <div class="col-md-6 col-xl-3">
             <div class="card widget-card-1">
                 <div class="card-block-small">
-                    <i class="feather icon-pie-chart bg-c-green card1-icon"></i>
+                    <i class="feather icon-user-check bg-c-green card1-icon"></i>
                     <span class="text-c-green f-w-600">Role</span>
                     <h4>{{ $widget->role }} Access</h4>
                     <div>
@@ -43,7 +43,7 @@
         <div class="col-md-6 col-xl-3">
             <div class="card widget-card-1">
                 <div class="card-block-small">
-                    <i class="feather icon-pie-chart bg-c-yellow card1-icon"></i>
+                    <i class="feather icon-user-plus bg-c-yellow card1-icon"></i>
                     <span class="text-c-yellow f-w-600">Users</span>
                     <h4>{{ $widget->user }} Users</h4>
                     <div>
@@ -57,7 +57,7 @@
         <div class="col-md-6 col-xl-3">
             <div class="card widget-card-1">
                 <div class="card-block-small">
-                    <i class="feather icon-pie-chart bg-c-blue card1-icon"></i>
+                    <i class="feather icon-home bg-c-blue card1-icon"></i>
                     <span class="text-c-blue f-w-600">Branch</span>
                     <h4>2 Branch</h4>
                     <div>
