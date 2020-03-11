50b90e7108fc0c63a9ccae1bbdfb420fdc2da0f6
Ibnu Surkati <dev.surkati@gmail.com>
2020-03-12 05:58:47 +0800

    add javascript

---
+++ public/js/pages/izin.js
@@ -0,0 +1,79 @@
+// submit form name="form-izin"
+$('form[name="form-izin"').on('submit', function(e) {
+    e.preventDefault()
+    
+    let form = $(this)
+    let data = form.serializeObject()
+    let url  = form.attr('action-link')
+    let type = form.attr('action-type')
+
+    if (type == 'create') {
+        $.created(url, data).then(() => {
+            $('#form-izin').modal('hide')
+            $.listdata('/izin')
+        })
+    } else {
+        $.updated(url, data).then(() => {
+            $('#form-izin').modal('hide')
+            $.listdata('/izin')
+        })
+    }
+})
+
+/**
+ * function edit izin
+ * @param id 
+ * @return @void
+ */
+function edit(id) {
+    var url = `/izin/${id}`
+    $.finddata(url + '/edit', 'form-izin').then(response => {
+        let form = $(`form[name="form-izin"]`)
+
+        form.attr('action-link', url)
+        form.attr('action-type', 'update')
+        
+        $.each(response.data, (key, val) => {
+            form.find(`[name="${key}"]`).val(val)
+        })
+        
+        $('#form-izin').modal('show').find('.modal-title').text('Update data izin')
+    })
+}
+
+/**
+ * function delete izin
+ * @param id 
+ * @return @void
+ */
+function deleted(id) {
+    Swal.fire({
+        title: 'Are you sure ?',
+        text: 'the data will go into the trash',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#1ABC9C',
+        cancelButtonColor: '#E74C3C',
+        confirmButtonText: 'Delete',
+        allowOutsideClick: false
+    }).then((result) => {
+        if (result.value) {
+            $.deleted(`/izin/${id}`).then(() => {
+                $.listdata('/izin')
+            })
+        }
+    })
+}
+
+// clear form izin
+$('#form-izin').on('hide.bs.modal', function () {
+    let modal = $(this)
+    let form  = modal.find('form[name="form-izin"]')
+    
+    modal.find('.modal-title').text('Create new izin')
+    
+    form.attr('action-link', '/izin')
+    form.attr('action-type', 'create')
+
+    form.find('[name]').val('')
+})
