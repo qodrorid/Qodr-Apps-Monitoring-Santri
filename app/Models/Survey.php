4c9a57f2dbdecda4593bd188f4fce60dee820ad1
Asmawi <imwashedup11@gmail.com>
2020-06-14 10:08:20 +0800

    new migrate and seeder

---
+++ app/Models/Survey.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Survey extends Model
+{
+    protected $table = 'surveys';
+
+    protected $fillable = [
+        'title',
+        'author_id',
+        'date_start',
+        'date_end',
+        'time_limit',
+        'note'
+    ];
+}
